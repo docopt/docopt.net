f471ae4e56ee474e253b2595d2fa457b6b389f8c
Dinh Doan Van Bien <dinh@doanvanbien.com>
2013-07-04 16:26:42 +0100

    Sync version with ref python implementation

--- src/NDocOpt/Properties/AssemblyInfo.cs
+++ src/NDocOpt/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.6.1.0")]
+[assembly: AssemblyFileVersion("0.6.1.0")]
 
 [assembly: InternalsVisibleTo("NDocOpt.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100915abcad70deeb2af0f6424bf23227473e27c84df5e57319274e9c8f5dba27363c2a6d93fa4883dbbc0a3bf18cdba7b677b9d02fa8ab885ca01adc30be2eec9d00e0655e7e1da16c307e2f3dfd95ad84e85b28765de915f2da1dc8d5f9f359614f2ee7838823a4a0cca9fd3d9425b231dabae00f46fbebcf5b5d6a0f5126efb5")]
