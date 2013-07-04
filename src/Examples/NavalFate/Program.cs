622f0181212d2e5c0a463f7044cd0224a5fd3336
Dinh Doan Van Bien <dinh@doanvanbien.com>
2013-07-04 17:09:03 +0100

    Added navalfate example

--- src/Examples/NavalFate/Program.cs
+++ src/Examples/NavalFate/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NDocOpt;
 
 namespace NavalFate
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private const string usage = @"Naval Fate.
+
+    Usage:
+      naval_fate.exe ship new <name>...
+      naval_fate.exe ship <name> move <x> <y> [--speed=<kn>]
+      naval_fate.exe ship shoot <x> <y>
+      naval_fate.exe mine (set|remove) <x> <y> [--moored | --drifting]
+      naval_fate.exe (-h | --help)
+      naval_fate.exe --version
+
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+      --speed=<kn>  Speed in knots [default: 10].
+      --moored      Moored (anchored) mine.
+      --drifting    Drifting mine.
+
+    ";
+
+        private static void Main(string[] args)
         {
+            var arguments = new DocOpt().Apply(usage, args, version: "Naval Fate 2.0");
+            foreach (var argument in arguments)
+            {
+                Console.WriteLine("{0} = {1}", argument.Key, argument.Value);
+            }
         }
     }
 }
