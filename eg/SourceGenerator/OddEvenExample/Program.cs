1cd008733095021138506f81ff5d2cfea36083ce
Atif Aziz <code@raboof.com>
2021-05-26 00:44:50 +0200

    Add port of "odd_even_example.py"
    https://github.com/docopt/docopt/blob/20b9c4ffec71d17cee9fd963238c8ec240905b65/examples/odd_even_example.py

---
+++ eg/SourceGenerator/OddEvenExample/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DocoptNet.Generated;
+using OddEvenExample;
+
+Program program;
+
+try
+{
+    program = new Program(args, exit: true);
+}
+catch (DocoptInputErrorException e)
+{
+    Console.Error.WriteLine(e);
+    return 0xbd;
+}
+
+foreach (var (name, value) in program.Args)
+    Console.WriteLine("{0} = {1}", name, value);
+
+Console.WriteLine($@"{{
+    Help = {program.OptHelp},
+    Odd  = [{string.Join(", ", from object v in program.ArgOdd select v)}],
+    Even = [{string.Join(", ", from object v in program.ArgEven select v)}],
+}}");
+
+return 0;
+
+namespace OddEvenExample
+{
+    partial class Program
+    {
+        readonly IDictionary<string, ValueObject> _args;
+
+        public Program(ICollection<string> argv, bool help = true,
+                       object version = null, bool optionsFirst = false, bool exit = false)
+        {
+            _args = Apply(argv, help, version, optionsFirst, exit);
+        }
+
+        public IDictionary<string, ValueObject> Args => _args;
+    }
+}
