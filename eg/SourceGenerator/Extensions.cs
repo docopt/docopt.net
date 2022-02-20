e594d87f3aef0a4ccd14c3d1355c2eab1886dee6
Atif Aziz <code@raboof.com>
2022-02-20 23:30:22 +0100

    Consolidate duplication in "ParseResultExtensions"

--- eg/SourceGenerator/Extensions.cs
+++ eg/SourceGenerator/Extensions.cs
@@ -11,72 +11,44 @@ static partial class ParseResultExtensions
 
     public static int Run<T>(this IHelpFeaturingParser<T> parser, IEnumerable<string> args,
                              TextWriter stdout, TextWriter stderr, int errorExitCode,
-                             Func<T, int> runner)
-    {
-        stdout ??= Console.Out;
-        stderr ??= Console.Error;
-
-        switch (parser.Parse(args))
-        {
-            case IArgumentsResult<T> r:
-                return runner(r.Arguments);
-            case IHelpResult r:
-                stdout.WriteLine(r.Help);
-                return 0;
-            case IInputErrorResult r:
-                stderr.WriteLine(r.Usage);
-                return errorExitCode != 0 ? errorExitCode : 1;
-            case var result:
-                throw new SwitchExpressionException(result);
-        }
-    }
+                             Func<T, int> runner) =>
+        Run(parser.Parse(args), stdout, stderr, errorExitCode, runner);
 
     public static int Run<T>(this IParser<T> parser, IEnumerable<string> args, Func<T, int> runner) =>
         parser.Run(args, null, null, 0, runner);
 
     public static int Run<T>(this IParser<T> parser, IEnumerable<string> args,
                              TextWriter stdout, TextWriter stderr, int errorExitCode,
-                             Func<T, int> runner)
-    {
-        stdout ??= Console.Out;
-        stderr ??= Console.Error;
-
-        switch (parser.Parse(args))
-        {
-            case IArgumentsResult<T> r:
-                return runner(r.Arguments);
-            case IHelpResult r:
-                stdout.WriteLine(r.Help);
-                return 0;
-            case IVersionResult r:
-                stdout.WriteLine(r.Version);
-                return 0;
-            case IInputErrorResult r:
-                stderr.WriteLine(r.Usage);
-                return errorExitCode != 0 ? errorExitCode : 1;
-            case var result:
-                throw new SwitchExpressionException(result);
-        }
-    }
+                             Func<T, int> runner) =>
+        Run(parser.Parse(args), stdout, stderr, errorExitCode, runner);
 
     public static int Run<T>(this IBaselineParser<T> parser, IEnumerable<string> args, Func<T, int> runner) =>
         parser.Run(args, null, 0, runner);
 
     public static int Run<T>(this IBaselineParser<T> parser, IEnumerable<string> args,
                              TextWriter stderr, int errorExitCode,
-                             Func<T, int> runner)
+                             Func<T, int> runner) =>
+        Run(parser.Parse(args), null, stderr, errorExitCode, runner);
+
+    static int Run<T>(object result,
+                      TextWriter stdout, TextWriter stderr, int errorExitCode,
+                      Func<T, int> runner)
     {
+        stdout ??= Console.Out;
         stderr ??= Console.Error;
 
-        switch (parser.Parse(args))
+        switch (result)
         {
             case IArgumentsResult<T> r:
                 return runner(r.Arguments);
+            case IHelpResult r:
+                stdout.WriteLine(r.Help);
+                return 0;
             case IInputErrorResult r:
                 stderr.WriteLine(r.Usage);
                 return errorExitCode != 0 ? errorExitCode : 1;
-            case var result:
-                throw new SwitchExpressionException(result);
+            case var unmatched:
+                throw new SwitchExpressionException(unmatched);
         }
     }
 }
