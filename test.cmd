8411ae67085e3d0568d626dc778161862f059632
Atif Aziz <code@raboof.com>
2022-03-30 22:21:31 +0200

    Improve test script error-handling

--- test.cmd
+++ test.cmd
@@ -1,13 +1,15 @@
 @echo off
 setlocal
 pushd "%~dp0"
-if not defined CI dotnet pack
+if not defined CI dotnet pack || goto :finally
+set INSTALLED=
 dotnet new --install src\ConsoleApp || goto :finally
+set INSTALLED=1
 dotnet new docopt-console -o tmp -n MyConsoleApp || goto :finally
 dotnet run --project tmp -- --help || goto :finally
 :finally
 set EXIT_CODE=%ERRORLEVEL%
 if exist tmp rmdir /s /q tmp
-dotnet new --uninstall src\ConsoleApp
+if defined INSTALLED dotnet new --uninstall src\ConsoleApp
 popd
 exit /b %EXIT_CODE%
