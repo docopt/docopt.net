375a3dad115cad3706e706051454b0e47aa724d8
Atif Aziz <code@raboof.com>
2022-12-28 10:46:27 +0100

    Simplify playground app with top-level statements

--- src/DocoptNet.Playground/Program.cs
+++ src/DocoptNet.Playground/Program.cs
@@ -1,23 +1,13 @@
 // Licensed under terms of MIT license (see LICENSE-MIT)
 // Copyright 2021 Atif Aziz, Dinh Doan Van Bien
 
-namespace DocoptNet.Playground
-{
-    using System;
-    using System.Net.Http;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-    using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-    static class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            var baseUrl = new Uri(builder.HostEnvironment.BaseAddress);
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = baseUrl });
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<DocoptNet.Playground.App>("#app");
+var baseUrl = new Uri(builder.HostEnvironment.BaseAddress);
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = baseUrl });
+await builder.Build().RunAsync();
