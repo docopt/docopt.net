1709262f0445d2f427a2d8f286948bf7631a5e2a
Atif Aziz <code@raboof.com>
2025-12-24 13:28:26 +0100

    Update to .NET 10

--- test.cmd
+++ test.cmd
@@ -3,13 +3,13 @@ setlocal
 pushd "%~dp0"
 if not defined CI dotnet pack || goto :finally
 set INSTALLED=
-dotnet new --install src\ConsoleApp || goto :finally
+dotnet new install src\ConsoleApp || goto :finally
 set INSTALLED=1
 dotnet new docopt-console -o tmp -n MyConsoleApp || goto :finally
 dotnet run --project tmp -- --help || goto :finally
 :finally
 set EXIT_CODE=%ERRORLEVEL%
 if exist tmp rmdir /s /q tmp
-if defined INSTALLED dotnet new --uninstall src\ConsoleApp
+if defined INSTALLED dotnet new uninstall src\ConsoleApp
 popd
 exit /b %EXIT_CODE%
